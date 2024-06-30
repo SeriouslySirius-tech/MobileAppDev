b53c97d94ad6e179d38e8e5d44ac61ae557f61ba
SeriouslySirius-tech <pranavy2157@gmail.com>
2024-06-30 18:58:57 +0530

    Updated dependencies and can read PDFs now

--- lib/file_display.dart
+++ lib/file_display.dart
@@ -1,8 +1,9 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle; // for assets
 import 'dart:io';
-import 'package:mad_project/models/file_object.dart'; // for external storage
+import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:mad_project/models/file_object.dart';
 
 class FileDisplayWidget extends StatefulWidget {
   final FileObject file; // Path to the file (asset or external storage)
@@ -14,62 +15,63 @@ class FileDisplayWidget extends StatefulWidget {
 }
 
 class _FileDisplayWidgetState extends State<FileDisplayWidget> {
-  Future<String>? _fileContent;
+  Future<String>? _localFilePath;
 
   @override
   void initState() {
     super.initState();
-    _fileContent = _readFile(widget.file.filePath);
+    _localFilePath = _getFilePath(widget.file.filePath);
   }
 
-  Future<String> _readFile(String filePath) async {
+  Future<String> _getFilePath(String filePath) async {
     if (filePath.startsWith('assets/')) {
-      return await rootBundle.loadString(filePath);
+      final byteData = await rootBundle.load(filePath);
+      final buffer = byteData.buffer;
+      final tempDir = await getTemporaryDirectory();
+      final tempFilePath = '${tempDir.path}/${widget.file.fileName}';
+      final file = await File(tempFilePath).writeAsBytes(
+          buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+      return file.path;
     } else {
-      final file = File(filePath);
-      return await file.readAsString();
+      return filePath;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<String>(
-      future: _fileContent,
+      future: _localFilePath,
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final lines = snapshot.data!.split('\n');
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(widget.file.fileName),
-              titleTextStyle: Theme.of(context)
-                  .primaryTextTheme
-                  .titleLarge!
-                  .copyWith(
-                      color: Theme.of(context).colorScheme.onPrimaryContainer),
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    lines.join('\n'),
-                    style: Theme.of(context)
-                        .primaryTextTheme
-                        .bodyLarge!
-                        .copyWith(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onPrimaryContainer),
-                  ),
-                ],
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(widget.file.fileName),
+                titleTextStyle: Theme.of(context)
+                    .primaryTextTheme
+                    .titleLarge!
+                    .copyWith(
+                        color:
+                            Theme.of(context).colorScheme.onPrimaryContainer),
               ),
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
+              body: PDFView(
+                filePath: widget.file.filePath,
+                nightMode: true,
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Scaffold(
+              body: Center(
+                child: Text('Error: ${snapshot.error}'),
+              ),
+            );
+          }
         }
-        return const CircularProgressIndicator();
+        return const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
       },
     );
   }
--- lib/providers/files.dart
+++ lib/providers/files.dart
@@ -1,9 +1,12 @@
+// import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mad_project/models/file_object.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:intl/intl.dart';
 import 'dart:typed_data';
+import 'package:pdf/widgets.dart' as pw;
+// import 'package:flutter/material.dart';
 
 final formatter = DateFormat('dd-MM-yy HH:mm');
 
@@ -25,21 +28,48 @@ class FilesNotifier extends StateNotifier<List<FileObject>> {
 
     // Create some files within the new directory
     for (int i = 1; i <= 3; i++) {
-      final file = File('${newDirectory.path}/file_$i.txt');
-      await file.writeAsString('This is file number $i');
+      final pdf = pw.Document();
+      pdf.addPage(
+        pw.Page(
+          build: (pw.Context context) {
+            return pw.Center(
+              child: pw.Text("This is file $i"),
+            );
+          },
+        ),
+      );
+      // final uniqueFileName = formatter.format(DateTime.now());
+      final file = File('${newDirectory.path}/file_$i.pdf');
+      if (!await file.exists()) {
+        await file.writeAsBytes(await pdf.save());
+      }
     }
 
+    // final file = File('${newDirectory.path}/file_$i.pdf');
+    // if (!await file.exists()) {
+    //   await file.writeAsString('This is file number $i', encoding: utf8);
+    // }
+
     // List all files in the directory and store FileInfo objects
+    // await for (var entity
+    //     in newDirectory.list(recursive: false, followLinks: false)) {
+    //   FileObject f = FileObject(
+    //     fileName: entity.uri.pathSegments.last,
+    //     filePath: entity.path,
+    //     date: formatter.format(DateTime.now()),
+    //   );
+    //   convertTextFileToPdf(f);
+    // }
+
     await for (var entity
         in newDirectory.list(recursive: false, followLinks: false)) {
       if (entity is File) {
-        state = [
-          ...state,
-          FileObject(
-              fileName: entity.uri.pathSegments.last,
-              filePath: entity.path,
-              date: formatter.format(DateTime.now()))
-        ];
+        FileObject f = FileObject(
+          fileName: entity.uri.pathSegments.last,
+          filePath: entity.path,
+          date: formatter.format(DateTime.now()),
+        );
+        state = [...state, f];
       }
     }
   }
@@ -60,6 +90,29 @@ class FilesNotifier extends StateNotifier<List<FileObject>> {
   void addDoc(FileObject f) {
     state = [...state, f];
   }
+
+//   Future<void> convertTextFileToPdf(FileObject file) async {
+//     final textFile = File(file.filePath);
+//     final textContentBytes = await textFile.readAsBytes();
+//     final textContent = utf8.decode(textContentBytes);
+//     final pdf = pw.Document();
+
+//     pdf.addPage(
+//       pw.Page(
+//         build: (pw.Context context) {
+//           return pw.Text(textContent);
+//         },
+//       ),
+//     );
+
+//     String pdfFilePath =
+//         '${file.filePath.substring(0, file.filePath.lastIndexOf('.') + 1)}pdf';
+//     final outputFile = File(pdfFilePath);
+//     await outputFile.writeAsBytes(await pdf.save());
+//     await textFile.delete();
+//     // print('PDF file created at: $pdfFilePath');
+//   }
+// }
 }
 
 final filesProvider =
